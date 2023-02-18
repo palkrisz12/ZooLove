b91bb38e0d83e2f54518ffd8682a51e3e05ab469
palkrisz12 <palkrisztian10@gmail.com>
2023-02-18 16:35:19 +0100

    Valami problema van php interpreteremmel

---
+++ web/welcome.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$uname = "admin";
+$pwd = "admin";
+
+session_start();
+
+if(isset($_SESSION['uname']))
+{
+    echo "<h1>Udvozlunk! ".$_SESSION['uname']."</h1>";
+    echo "<a href='product.php'>Termek</a><br>";
+    echo "<br><a href='logout.php'><input type=button value=logout name=logout></a>";
+}
+else{
+    if($_POST['uname']==$uname && $_POST['pwd']==$pwd){
+    $_SESSION['uname']=$uname;
+
+    echo "<script> location.href = 'welcome.php' </script>";
+    }else{
+        echo"<script>alert('A jelszo vagy a felhasznalonev helytelen!')</script>";
+
+        echo "<script> location.href = 'login.php' </script>";
+
+    }
+
+}
+
+
+
+?>
