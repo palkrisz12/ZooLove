06b47de12aca1125b05c2a0c5880fcaa882f0ef2
palkrisz12 <119298440+palkrisz12@users.noreply.github.com>
2023-03-26 19:39:26 +0200

    Naaa, ezen valamiert mukodik minden,

--- web/welcome.php
+++ web/welcome.php
@@ -1,31 +1,26 @@
 <?php
+// Connect to your database
+$db = mysqli_connect('localhost', 'username', '', 'users');
 
+// Check if the user has submitted the login form
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the user's login credentials from the form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$uname = "admin";
-$pwd = "admin";
+    // Query the database for the user with the given credentials
+    $query = "SELECT id FROM users WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($db, $query);
 
-session_start();
-
-if(isset($_SESSION['uname']))
-{
-    echo "<h1>Udvozlunk! ".$_SESSION['uname']."</h1>";
-    echo "<a href='product.php'>Termek</a><br>";
-    echo "<br><a href='logout.php'><input type=button value=logout name=logout></a>";
-}
-else{
-    if($_POST['uname']==$uname && $_POST['pwd']==$pwd){
-    $_SESSION['uname']=$uname;
-
-    echo "<script> location.href = 'welcome.php' </script>";
-    }else{
-        echo"<script>alert('A jelszo vagy a felhasznalonev helytelen!')</script>";
-
-        echo "<script> location.href = 'login.php' </script>";
-
+    // If a matching user is found, set a login session for them
+    if (mysqli_num_rows($result) === 1) {
+        session_start();
+        $_SESSION['user_id'] = mysqli_fetch_assoc($result)['id'];
+        header('Location: dashboard.php');
+        exit;
     }
-
+    else {
+        $error = 'Invalid username or password';
+    }
 }
-
-
-
 ?>
